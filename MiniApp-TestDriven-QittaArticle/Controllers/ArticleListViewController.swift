2994818df00724d04a65340638ad00f79205db13
mikekondo <foad5969@gmail.com>
2022-10-04 14:09:49 +0900

    add: 通信処理実装

---
+++ MiniApp-TestDriven-QittaArticle/Controllers/ArticleListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleListViewController.swift
+//  MiniApp-TestDriven-QittaArticle
+//
+//  Created by 近藤米功 on 2022/10/04.
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // titleLabel.text = "記事タイトル"
+    }
+}
---
+++ MiniApp-TestDriven-QittaArticle/Protocol/ArticleListAPIClientProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ArticleListAPIClientProtocol.swift
+//  MiniApp-TestDriven-QittaArticle
+//
+//  Created by 近藤米功 on 2022/10/04.
+//
+
+import Foundation

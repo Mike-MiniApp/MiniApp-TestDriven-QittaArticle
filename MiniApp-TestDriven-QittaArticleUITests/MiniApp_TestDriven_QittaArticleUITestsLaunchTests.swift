66369ca2836a844965fc5783ed244636002b019f
mikekondo <foad5969@gmail.com>
2022-10-04 11:27:40 +0900

    add: firstcommit

---
+++ MiniApp-TestDriven-QittaArticleUITests/MiniApp_TestDriven_QittaArticleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MiniApp_TestDriven_QittaArticleUITestsLaunchTests.swift
+//  MiniApp-TestDriven-QittaArticleUITests
+//
+//  Created by 近藤米功 on 2022/10/04.
+//
+
+import XCTest
+
+final class MiniApp_TestDriven_QittaArticleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
